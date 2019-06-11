fc8f9f640523f0edd16409d4f415f8f1a68beb8c
Ella <michaella.latkovic@gmail.com>
2019-06-11 16:24:04 -0700

    Added migration to add column to recipes table

---
+++ db/migrate/20190611231333_add_column_to_recipes_table.rb
@@ -0,0 +1,5 @@
+class AddColumnToRecipesTable < ActiveRecord::Migration
+  def change
+     add_column :recipes, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190611171045) do
+ActiveRecord::Schema.define(version: 20190611231333) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190611171045) do
     t.string  "ingredients"
     t.string  "directions"
     t.integer "cook_time"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
