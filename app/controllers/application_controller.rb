3c0e686cd40b56228ec29c2293c457baf6ef42f2
Ella <michaella.latkovic@gmail.com>
2019-06-12 16:46:30 -0700

    Added helper methods and enabled sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def current_user(session)
+      User.find(session[:user_id])
+    end
+
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end
   end
 
 end
