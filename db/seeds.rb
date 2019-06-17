19b3b91584e35834b0a4ae2fb2b8594f40eb0047
Ella <michaella.latkovic@gmail.com>
2019-06-17 15:41:16 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+Recipe.delete_all
+Category.delete_all
+User.delete_all
 
-ella = User.create(:username=>'Ella', :email=>'ella@gmail.com', :password_digest=>'123', :city=>'Seattle', :state=>'WA')
-nate = User.create(:username=>'Nate', :email=>'nate@gmail.com', :password_digest=>'456', :city=>'Boston', :state=>'MA')
+
+snack = Category.create(:name=>'Snack')
+healthy = Category.create(:name=>'Healthy')
+breakfast = Category.create(:name=>'Breakfast')
+dinner = Category.create(:name=>'Dinner')
+meal = Category.create(:name=>'Meal')
