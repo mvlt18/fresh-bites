49a88025f95552b4889664e7ddbcab1cae2cfc27
Ella <michaella.latkovic@gmail.com>
2019-06-17 15:43:16 -0700

    Added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :recipes
   has_many :categories, through: :recipes
 
+  validates :email, uniqueness: true
+
+  has_secure_password
+
   #the gsub replaces spaces with hyphens, downcase makes it lowercase.
   def slug
     self.username.gsub(" ", "-").downcase
